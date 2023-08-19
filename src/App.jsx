7922c9648779a0aaca71b186a8e02b07fc1e0bff
Javoc <javier.ochoa.77@gmail.com>
2023-08-19 19:29:04 -0400

    se importa bootstrap y bbdd en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BaseColaboradores } from './database/BaseColaboradores';
 
 function App() {
   return (
