0f1e1a95d04d3b6dad85893413e8b9a52b6cf45d
Rcamino1 <127453851+Rcamino1@users.noreply.github.com>
2023-08-21 13:38:40 -0400

    Se agrega el formulario y sus funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BaseColaboradores } from "./database/BaseColaboradores";
 import Listado from "./components/Listado";
+import Formulario from "./components/Formulario"
 
 function App() {
   return (
     <>
       <Listado collabs={BaseColaboradores} />
+      <Formulario/>
     </>
   );
 }
